55590bed26c52c49a75b9a4327e40737f0c4e7df
Jesper <jesperoka@gmail.com>
2022-11-19 02:40:40 +0100

    Started on first two scenes

--- src/oneMinuteVideo.py
+++ src/oneMinuteVideo.py
@@ -5,34 +5,26 @@ else:
     import manimlib as m
 
     def scene1(self):
-        circle = m.Circle()
-        circle.set_fill(m.BLUE, opacity=0.5)
-        circle.set_stroke(m.BLUE_E, width=4)
-
-        # self.add(circle)
-
-        square = m.Square()
-
-        self.play(m.ShowCreation(square))
+        text = m.Text("Feedback Loop", font="Copperplate").scale(2)
+        # text.arrange(m.RIGHT, buff=m.MED_SMALL_BUFF)
+        self.play(m.Write(text), run_time=1)
         self.wait()
-        self.play(m.ReplacementTransform(square, circle))
-        self.wait()
+        return text
+        
+    def scene2(self,  text):
+        line = m.Line().scale(2.5).shift(1.1*m.UP)
+        roundedRectangle = m.RoundedRectangle(color=m.ORANGE).scale(1.3).flip().shift(0.2*m.DOWN)
+        transform1 = text.animate.shift(2*m.UP).scale(0.5)
+        transform2 = m.FadeIn(line, run_time=2)
+        transform3 = m.FadeToColor(text[8:], m.ORANGE)
+        transform4 = m.FadeToColor(line, m.ORANGE)
+        transform5 = m.Transform(line, roundedRectangle)
+        # transform5 = m.ShowCreation(roundedRectangle)
+        self.play(transform1, transform2)
+        self.play(transform3, transform4)
+        self.play(transform5)
 
-    def scene2(self):
-        circle = m.Circle()
-        circle.set_fill(m.BLUE, opacity=0.5)
-        circle.set_stroke(m.BLUE_E, width=4)
-
-        # self.add(circle)
-
-        square = m.Square()
-
-        self.play(m.ShowCreation(square))
-        self.wait()
-        self.play(m.ReplacementTransform(square, circle))
-        self.wait()
-
-    class OneMinuteVideo(m.Scene):
+    class OneMinuteVideo(m.Scene): 
         def construct(self):
-            scene1(self)
-            scene2(self)
+            text = scene1(self)
+            scene2(self, text)
